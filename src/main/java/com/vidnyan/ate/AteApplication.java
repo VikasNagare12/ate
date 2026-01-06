5471cd8a9d25e315b9ac9dc64d8fd53bb76823c1
VikasNagare <vikasnagare05@gmail.com>
2026-01-06 21:29:36 +0530

    chore: Remove old code - keep only clean architecture v2
    Removed:
    - model/ (old Type, Method, Field, etc.)
    - graph/ (old CallGraph, DependencyGraph)
    - advisor/ (old Advisor implementations)
    - parser/ (old RepositoryScanner)
    - report/ (old Report classes)
    - rule/ (old evaluators)
    - AnalysisEngine.java
    - AnalysisProperties.java
    
    This branch now contains only the new clean architecture code.
    Old code is preserved in main branch.

--- src/main/java/com/vidnyan/ate/AteApplication.java
+++ src/main/java/com/vidnyan/ate/AteApplication.java
@@ -2,10 +2,8 @@ package com.vidnyan.ate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AnalysisProperties.class)
 public class AteApplication {
 
 	public static void main(String[] args) {
